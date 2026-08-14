8e1f5aa593c54cef582ab144c0c1e392938435c6
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-08-14 10:49:54 +0530

    Create 76-MinimumWindowSubstring

---
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
@@ -0,0 +1,47 @@
+// Last updated: 8/14/2026, 10:53:15 AM
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        int[] freq = new int[128];
+
+        for (char c : t.toCharArray()) {
+            freq[c]++;
+        }
+
+        int left = 0;
+        int count = t.length();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            if (freq[ch] > 0) {
+                count--;
+            }
+            freq[ch]--;
+
+            while (count == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                freq[leftChar]++;
+
+                if (freq[leftChar] > 0) {
+                    count++;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+}
