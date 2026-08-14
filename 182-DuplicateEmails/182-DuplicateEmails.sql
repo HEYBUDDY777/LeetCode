38903bede180b47c8199a2793000b19d47802db8
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-08-14 10:48:43 +0530

    Create 182-DuplicateEmails

---
+++ 182-DuplicateEmails/182-DuplicateEmails.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/14/2026, 10:52:04 AM
+# Write your MySQL query statement below
+select distinct p1.email
+from person p1
+join person p2
+on p1.email = p2.email
+and p1.id <> p2.id;
