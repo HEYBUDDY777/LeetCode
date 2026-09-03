2978824cf1338aeb61e537010b27b6271ad1f0bc
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-09-03 12:39:43 +0530

    File updated at 9/3/2026, 12:40:36 PM

--- 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.java
+++ 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.java
@@ -1,6 +1,27 @@
-// Last updated: 9/3/2026, 12:28:00 PM
+// Last updated: 9/3/2026, 12:40:36 PM
 1class Solution {
 2    public boolean uniformArray(int[] nums1) {
-3        return true;
-4    }
-5}
+3        int even = 0;
+4        int odd  = 0;
+5        int n = nums1.length;
+6        int emin = Integer.MAX_VALUE;
+7        int omin = Integer.MAX_VALUE;
+8        for(int i=0;i<n;i++)
+9        {
+10            if(nums1[i]%2==0)
+11            {
+12                even++;
+13                emin = Math.min(emin,nums1[i]);
+14            }
+15            else{
+16                odd++;
+17                omin = Math.min(omin,nums1[i]);
+18            }
+19        }
+20        if(odd==n || even==n ||emin>omin)
+21        {
+22            return true;
+23        }
+24        return false;
+25    }
+26}
