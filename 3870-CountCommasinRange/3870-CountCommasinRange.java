d052d2838e079634f39265262ec25110ac6690d8
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-09-08 10:06:27 +0530

    File updated at 9/8/2026, 10:07:26 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,11 +1,10 @@
-// Last updated: 9/8/2026, 10:06:15 AM
+// Last updated: 9/8/2026, 10:07:26 AM
 1class Solution {
 2    public int countCommas(int n) {
-3        int sum = 0;
-4        for(int i=1000;i<=n;i++)
-5        {
-6            sum++;
-7        }
-8        return sum;
-9    }
-10}
+3        if(n<1000)
+4        {
+5            return 0;
+6        }
+7        return n-999;
+8    }
+9}
