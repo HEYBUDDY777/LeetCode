0feb2c13a1f2201805ab314489c254869f15347a
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-09-09 12:03:43 +0530

    Create 3870-CountCommasInRange

---
+++ 3870-CountCommasInRange/3870-CountCommasInRange.java
@@ -0,0 +1,10 @@
+// Last updated: 9/9/2026, 12:04:42 PM
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000)
+        {
+            return 0;
+        }
+        return n-999;
+    }
+}
