900261a2951cb45a5ad1955319610fa147b5035b
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-09-09 12:06:03 +0530

    Create 950-RevealCardsInIncreasingOrder

---
+++ 950-RevealCardsInIncreasingOrder/950-RevealCardsInIncreasingOrder.java
@@ -0,0 +1,21 @@
+// Last updated: 9/9/2026, 12:07:02 PM
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Arrays.sort(deck);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            q.offer(i);
+        }
+        int[] ans = new int[n];
+        for(int card :deck){  // 2 
+            int index = q.poll(); // 0
+            ans[index] = card;
+            if(!q.isEmpty()){
+                q.offer(q.poll());
+            }
+            
+        }
+        return ans;
+    }
+}
