acbd68174f46ba3feeb6744da69507c374f1df6f
Saravana Kumar S <saravanakumar35771@gmail.com>
2026-09-09 12:04:31 +0530

    Create 2073-TimeNeededToBuyTickets

---
+++ 2073-TimeNeededToBuyTickets/2073-TimeNeededToBuyTickets.java
@@ -0,0 +1,32 @@
+// Last updated: 9/9/2026, 12:05:30 PM
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<tickets.length;i++)
+        {
+            q.offer(new int[]{i,tickets[i]});
+        }
+        int time = 0;
+        while(!q.isEmpty())
+        {
+            int[] person = q.poll();
+            int index = person[0];
+            int rem = person[1];
+            rem--;
+            time++;
+            if(rem==0)
+            {
+                if(index==k)
+                {
+                    return time;
+                }
+            }
+            else{
+                q.offer(new int[]{index,rem});
+            }
+        }
+        return time;
+    
+    }
+}
